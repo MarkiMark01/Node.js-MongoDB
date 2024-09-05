60b7dee812907b4a95b1ba6c706b525317aa8e18
MarkiMark01 <markishgroup@gmail.com>
2024-09-05 20:15:44 +0300

    created server.js

--- App.js
+++ App.js
@@ -5,9 +5,10 @@ const booksRouter = require("./routes/api/books");
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 
 app.use("/api/books", booksRouter);
 
-app.listen(3000, () => console.log("Server running"));
+module.exports = app;
+
 
