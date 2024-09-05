36aa0ffb74c65bc7a2a3fd41f8cd6b2bc63dbc23
MarkiMark01 <markishgroup@gmail.com>
2024-09-05 19:44:30 +0300

    added cors to the project

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+const corsMiddleware = cors();
+
+app.use(corsMiddleware);
 
 app.listen(3000, () => console.log("Server running"));
 
