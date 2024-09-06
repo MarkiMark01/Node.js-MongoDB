d75dc693e79a8f281681ba52490f7a507ec7b80a
MarkiMark01 <markishgroup@gmail.com>
2024-09-06 15:23:07 +0300

    added fn 500 to app

--- App.js
+++ App.js
@@ -10,9 +10,13 @@ app.use(cors());
 app.use("/api/books", booksRouter);
 
 app.use((req, res) => {
-    res.status(404).json({message: 'Not found'})
+    res.status(404).json({ message: 'Not found' })
 });
 
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Server error' } = err;
+    res.status(status).json({message})
+})
 
 module.exports = app;
 
