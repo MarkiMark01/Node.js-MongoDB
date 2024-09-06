c0b185cd539eca18d65cba88171d2bd6f2d11bb1
MarkiMark01 <markishgroup@gmail.com>
2024-09-06 14:52:49 +0300

    updated router.get(/) in books

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,19 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    try {
+        const result = await books.getAll();
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+});
+router.get("/:id", async (req, res) => {
     res.json()
 });
-router.get("/:id", (req, res) => {
+router.post("/", async (req, res) => {
     res.json()
 });
-router.post("/", (req, res) => {
+router.put("/:id", async (req, res) => {
     res.json()
 });
-router.put("/:id", (req, res) => {
-    res.json()
-});
-router.delete("/", (req, res) => {
+router.delete("/", async (req, res) => {
     res.json()
 })
 
