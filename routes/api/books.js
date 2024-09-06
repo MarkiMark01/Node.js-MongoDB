e414c731c4b6768d70130c82e8d6b9f2dc78ac9a
MarkiMark01 <markishgroup@gmail.com>
2024-09-06 15:16:22 +0300

    updated router.get(/) in books

--- routes/api/books.js
+++ routes/api/books.js
@@ -9,12 +9,10 @@ router.get("/", async (req, res, next) => {
         const result = await books.getAll();
         res.json(result);
     } catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        })
+        next(error)
     }
 });
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
     try {
         const {id} = req.params;
         const result = await books.getById(id);
