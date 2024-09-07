6d69707e26bf6e7fbcc3b93a61878be16ebb675d
MarkiMark01 <markishgroup@gmail.com>
2024-09-07 18:19:04 +0300

    created getAll in controllers/books.js

--- controllers/books.js
+++ controllers/books.js
@@ -1,3 +1,21 @@
 const Joi = require("joi");
 
-const {HttpError} = require("../../helpers");
+const {HttpError} = require("../helpers");
+
+const addSchema = Joi.object({
+    title: Joi.string().required(),
+    author: Joi.string.required(),
+})
+
+const getAll = async (req, res, next) => {
+    try {
+        const result = await books.getAll();
+        res.json(result);
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    getAll,
+}
