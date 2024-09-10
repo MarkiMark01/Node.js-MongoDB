e0ba292e090636f127775645ec33d6f93099a899
MarkiMark01 <markishgroup@gmail.com>
2024-09-10 15:33:08 +0300

    created books in schemas

--- controllers/books.js
+++ controllers/books.js
@@ -22,10 +22,7 @@ const getById = async (req, res) => {
 }
 
 const add = async (req, res) => {
-        const {error} = addSchema.validate(req.body)
-        if(error){
-            throw HttpError(400, error.message);
-        }
+       
         const result = await books.add(req.body);
         res.status(201).json(result);
 }
