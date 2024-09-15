8ecea66c0490737612761ef39ef1ce04d64f6622
MarkiMark01 <markishgroup@gmail.com>
2024-09-15 14:58:49 +0300

    added userSchema to models/user

--- models/user.js
+++ models/user.js
@@ -5,3 +5,6 @@ const {handleMongooseError} = require("../helpers");
 
 const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+const userSchema = new Schema({
+
+}, {versionKey: false, timestamps: true});
