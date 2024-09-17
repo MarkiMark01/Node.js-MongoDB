971f551274ffed922e7c84662b55fffe2f7bfea6
MarkiMark01 <markishgroup@gmail.com>
2024-09-17 13:23:23 +0300

    created register in controllers/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,15 @@
-const {HttpError, ctrlWrapper} = require("../helpers");
-const {User} = require("../models/user")
+const { HttpError, ctrlWrapper } = require("../helpers");
+const { User } = require("../models/user")
+
+const register = async (req, res) => {
+    const newUser = await User.create(req.body);
+
+    res.json({
+        email: newUser.email,
+        name: newUser.name,
+    })
+}
+
+module.exports = {
+    register: ctrlWrapper(register),
+}
