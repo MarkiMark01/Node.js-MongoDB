777933fdff713e6b0c65fe447a9949ffea5933d1
MarkiMark01 <markishgroup@gmail.com>
2024-09-18 09:39:27 +0300

    added bcrypt to controllers/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,20 +4,21 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 const { User } = require("../models/user")
 
 const register = async (req, res) => {
-const {email, password} = req.body;
-const user = await User.findOne({email})
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
 
-if(user){
-    throw HttpError(409, "Email already in use");
-}
+    if (user) {
+        throw HttpError(409, "Email already in use");
+    }
 
+const hashPassword = await bcrypt.hash(password, 10);
 
-
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({...req.body, password: hashPassword});
 
     res.status(201).json({
         email: newUser.email,
         name: newUser.name,
+        
     })
 }
 
