5059c35e87d34bab929b18d871784fdfda24d298
MarkiMark01 <markishgroup@gmail.com>
2024-09-18 11:28:44 +0300

    created router.post/login in api/auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,7 @@ const ctrl = require("../../controllers/auth");
 const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 
 module.exports = router;
