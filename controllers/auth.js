54ddf345e6367a9093a305a72b26af2be30ceac1
MarkiMark01 <markishgroup@gmail.com>
2024-09-20 13:10:57 +0300

    created getCurrent in controllers/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { User } = require("../models/user")
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
     const { email, password } = req.body;
@@ -16,18 +16,18 @@ const register = async (req, res) => {
         throw HttpError(409, "Email already in use");
     }
 
-const hashPassword = await bcrypt.hash(password, 10);
+    const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const newUser = await User.create({ ...req.body, password: hashPassword });
 
     res.status(201).json({
         email: newUser.email,
         name: newUser.name,
-        
+
     })
 };
 
-const login = async(req, res) => {
+const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email })
     if (!user) {
@@ -35,23 +35,33 @@ const login = async(req, res) => {
     }
 
     const passwordCompare = await bcrypt.compare(password, user.password)
-if(!passwordCompare){
-    throw HttpError(401, "Email or password invalid");
+    if (!passwordCompare) {
+        throw HttpError(401, "Email or password invalid");
+    }
+
+    const payload = {
+        id: user._id,
+    }
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" })
+
+    res.json({
+        token,
+    })
+
 }
 
-const payload = {
-    id: user._id,
-}
+const getCurrent = async (req, res) => {
+    const { email, name } = req.user;
 
-const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"})
-
-res.json({
-    token,
-})
-
+    res.json({
+        email,
+        name,
+    })
 }
 
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
 }
