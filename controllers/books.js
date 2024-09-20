b514c05f344c7cb88ad97d249fcd1d78eab6f6fb
MarkiMark01 <markishgroup@gmail.com>
2024-09-20 13:03:18 +0300

    updated getAll in controllers/books

--- controllers/books.js
+++ controllers/books.js
@@ -2,8 +2,10 @@ const {HttpError, ctrlWrapper} = require("../helpers");
 const {Book} = require("../models/book");
 
 const getAll = async (req, res) => {
-    const {_id: owner} = req.user
-    const result = await Book.find({owner}, "-createdAt, -updateAt");
+    const {_id: owner} = req.user;
+    const {page = 1, limit = 10} = req.query;
+    const skip = (page - 1) * limit;
+    const result = await Book.find({owner}, "-createdAt, -updateAt", {skip, limit}).populate('owner', 'name email');
     res.json(result);
 }
 
